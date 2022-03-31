1aaa82b2b9ddb6aa7de102620f1b573a55e1b139
Andreas Hafner <git@hafner-online.net>
2022-03-31 20:58:24 +0200

    add build script and stuff to gen an standard iso

--- build-iso.sh
+++ build-iso.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
-project=$(basename `pwd`)
-sudo mkarchiso -v -w work $project
+PROJECT=gorillalinuxiso
+
+# TODO: make all changes before build iso
+# .
+# .
+# .
+
+# Make final iso directory
+if [ -d "$PROJECT" ]; then rm -Rf $PROJECT; fi
+mkdir gorillalinuxiso
+
+# Copy original archiso files to final iso directory
+cp -r ./archiso/* ./gorillalinuxiso/
+
+# Copy splash.png to final iso directory
+cp ./assets/images/splash.png ./gorillalinuxiso/syslinux/splash.png
+
+# Copy files that should changed compared to the original archiso into final iso directory.
+cp -r ./files-to-change-in-iso/* ./gorillalinuxiso/
+
+# Generate iso
+sudo mkarchiso -v -w work $PROJECT
